1734cf3c2b03a93f44b7164fac8faad3134a7b87
johnmosv <46477624+johnmosv@users.noreply.github.com>
2023-03-02 13:40:23 +0100

    added trans_dt

--- R/transpose_aggregate_dt.R
+++ R/transpose_aggregate_dt.R
@@ -14,3 +14,42 @@ transpose_aggregated_dt <- function(table1) {
   table_trans <- table_trans[-1]
   return(table_trans)
 }
+
+#' @title Transpose as wide dt
+#' @description Format it like a proper long table
+#' @param x A table with variables in columns and groups as rows
+#' @return A table with variables as rows and groups as columns
+#' @examples
+#' \dontrun{
+#' if (interactive()) {
+#'   # EXAMPLE1
+#' }
+#' }
+#' @seealso
+#'  \code{\link[dplyr]{pull}}, \code{\link[dplyr]{bind_cols}}
+#'  \code{\link[tibble]{tibble}}
+#' @rdname trans_dt
+#' @export
+#' @importFrom dplyr pull bind_cols
+#' @importFrom tibble tibble
+
+trans_dt <- function(x) {
+  first_col <- dplyr::pull(x[, 1])
+  if (is.logical(first_col) || is.numeric(first_col)) {
+    first_char <- as.character(first_col)
+    x[, 1] <- paste0(colnames(x)[1], "=", first_char)
+  }
+  tx <- t(x)
+  if (ncol(tx) == 1) {
+    tab <- tibble::tibble(var = rownames(tx), value = tx[, 1]) # nolint
+  }
+  if (ncol(tx) >= 2) {
+    first_table <- tibble::tibble(var = rownames(tx))
+    second_table <- as.data.frame(tx)
+    tab <- dplyr::bind_cols(first_table, second_table)
+    clnms <- as.character(unlist(tab[1, ]))
+    colnames(tab) <- clnms
+    tab <- tab[-1, ]
+  }
+  return(tab)
+}
