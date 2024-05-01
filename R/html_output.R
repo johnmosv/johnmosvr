f0cc2832c19e956f2f942cd724ac33dd39411891
johnmosv <46477624+johnmosv@users.noreply.github.com>
2024-05-01 20:19:36 +0200

    table_dt row_groups handle numeric values and detect html_output

--- R/html_output.R
+++ R/html_output.R
@@ -8,11 +8,11 @@
 #' @param row_groups Should first columns be grouped?
 #' @param alignment Alignment of cells c("dt-center", "dt-right", "dt-left")
 #' @param align_targets Numeric vector containing columns positions to
-#` apply alignment to
+# ` apply alignment to
 #' @param page_length Number of rows per page
 #' @param class Classes to apply. Defaults to "compact stripe"
 #' @param row_callback Row callback to apply.
-#` Default to changing the color of every other row.
+# ` Default to changing the color of every other row.
 #' @param width Width as proportion of parent div. Based on ncol bby default
 #' @param ... Additional variables to pass to datatable::DT
 #'
@@ -21,8 +21,12 @@
 #' @export
 table_dt <- function(data, col_names = NULL, first_colname = NULL,
                      title_row_names = TRUE, title_col_names = TRUE,
-                     row_groups = FALSE, alignment = "dt-center", align_targets = NULL, #nolint
-                     page_length = 20, class = NULL, row_callback = NULL, width = NULL, ...) { #nolint
+                     row_groups = FALSE, alignment = "dt-center", align_targets = NULL, # nolint
+                     page_length = 20, class = NULL, row_callback = NULL, width = NULL, ...) { # nolint
+
+  if (!knitr::is_html_output()) {
+    return(print(data, n = Inf))
+  }
   data <- dplyr::ungroup(data)
   dom_settings <- "tB"
 
@@ -31,11 +35,12 @@ table_dt <- function(data, col_names = NULL, first_colname = NULL,
   }
 
   if (title_col_names) {
-    col_names <- stringr::str_to_title(stringr::str_replace_all(col_names, "_", " ")) #nolint
+    col_names <- stringr::str_to_title(stringr::str_replace_all(col_names, "_", " ")) # nolint
   }
 
   if (row_groups) {
     if (is.factor(data[[1]])) data[[1]] <- as.character(data[[1]])
+    data[[1]] <- paste(data[[1]])
     data[duplicated(data[, 1]), 1] <- " "
   }
 
@@ -131,8 +136,8 @@ table_dt <- function(data, col_names = NULL, first_colname = NULL,
       )
     ),
     ...
-  ) 
-  return_table <- DT::formatStyle(return_table, columns = 1, color = "black", fontWeight = "bold") #nolint
+  )
+  return_table <- DT::formatStyle(return_table, columns = 1, color = "black", fontWeight = "bold") # nolint
 
   if ("percent" %in% colnames(data)) {
     return_table <- DT::formatPercentage(return_table, "percent")
@@ -152,13 +157,13 @@ table_dt <- function(data, col_names = NULL, first_colname = NULL,
 #'
 #' @export
 colorize_span <- function(text, color = "red") {
-    if (knitr::is_latex_output()) {
-        sprintf("\\textcolor{%s}{%s}", color, text)
-    } else if (knitr::is_html_output()) {
-        sprintf("<span style='color: %s;'>%s</span>", color, text)
-    } else {
-        text
-    }
+  if (knitr::is_latex_output()) {
+    sprintf("\\textcolor{%s}{%s}", color, text)
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='color: %s;'>%s</span>", color, text)
+  } else {
+    text
+  }
 }
 #' tabyl_dt: Call table_dt after running janitor::tabyl() of data
 #'
@@ -171,20 +176,20 @@ colorize_span <- function(text, color = "red") {
 #'
 #' @export
 tabyl_dt <- function(data, var1, var2 = NULL, ...) {
-    res <- janitor::tabyl(data, {{ var1 }})
-    if (!is.null(rlang::expr({{ var2 }}))) {
-        res <- janitor::tabyl(data, {{ var1 }}, {{ var2 }})
-    }
-    if (knitr::is_html_output()) {
-        return_table <- table_dt(res, , title_row_names = FALSE, title_col_names = FALSE, ...) #nolint
+  res <- janitor::tabyl(data, {{ var1 }})
+  if (!is.null(rlang::expr({{ var2 }}))) {
+    res <- janitor::tabyl(data, {{ var1 }}, {{ var2 }})
+  }
+  if (knitr::is_html_output()) {
+    return_table <- table_dt(res, , title_row_names = FALSE, title_col_names = FALSE, ...) # nolint
 
-        percent_cols <- c("percent", "valid_percent")
-        percent_cols_present <- percent_cols %in% colnames(res)
-        if (any(percent_cols_present)) {
-            percent_cols <- percent_cols[percent_cols_present]
-            return_table <- return_table %>% DT::formatPercentage(columns = "percent") #nolint
-        }
-        return(return_table)
+    percent_cols <- c("percent", "valid_percent")
+    percent_cols_present <- percent_cols %in% colnames(res)
+    if (any(percent_cols_present)) {
+      percent_cols <- percent_cols[percent_cols_present]
+      return_table <- return_table %>% DT::formatPercentage(columns = "percent") # nolint
     }
-    return(DT::datatable(res))
+    return(return_table)
+  }
+  return(DT::datatable(res))
 }
